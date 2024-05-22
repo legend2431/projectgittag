844909bdf3c00bb399c7519f493482b459cab91d
NKunwar <naimish.nk@gmail.com>
2024-05-22 15:20:55 +0530

    branch_two with tag v4

---
+++ Operators.java
@@ -0,0 +1,7 @@
+public class Operators {
+  public static void main(String[] args) {
+    int x = 100 + 50;
+    System.out.println(x);
+  }
+}
+// this will have tag v4
--- Variables.java
+++
@@ -1,7 +0,0 @@
-public class Variables {
-  public static void main(String[] args) {
-    String name = "John";
-    System.out.println(name);
-  }
-}
-// this will have tag v3
